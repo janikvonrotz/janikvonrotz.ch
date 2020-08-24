8eb38ae49aa7956f0b9a00a495dc6b93d6d96bb1
Janik von Rotz <login@janikvonrotz.ch>
2020-08-24 09:58:16 +0200

    update theme

---
+++ themes/hugo-lithium-theme/static/js/main.js
@@ -0,0 +1,7 @@
+document.getElementById('open-nav').onclick = () => {
+  document.getElementById('navigation').style.height = '100%'
+}
+
+document.getElementById('navigation').onclick = () => {
+  document.getElementById('navigation').style.height = '0%'
+}
